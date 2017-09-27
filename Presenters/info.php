68581b9cc814490de9f41700850535070d884f7a
meinvbingyue <meinvbingyue@qq.com>
2017-09-27 11:18:05 +0800

    Update info.php

--- Presenters/info.php
+++ Presenters/info.php
@@ -1,5 +1,9 @@
-引入：
+
+```php
+// 引入：
+<?php $appPresenter = app('\App\Presenters\AppPresenter'); ?>
 @inject('appPresenter','App\Presenters\AppPresenter')
 
-使用：
+// 使用：
 {{ $appPresenter->activeMenuByController('CategoryController') }}
+```
